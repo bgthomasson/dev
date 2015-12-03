14cbdea7c1c8db73f25eecf5aec40647ea2ea444
bgthomasson <benthomonster@gmail.com>
2015-12-02 18:03:06 -0600

    bloop

--- js/addClass.js
+++ js/addClass.js
@@ -1,4 +1,5 @@
-// a simple AJAXian script to change bg color on clicked buttons. that's it. 
+// a simple AJAXian script to change bg color on clicked buttons. 
+// We also need to change tense, so "like" becomes "liked", etc. 
 
 var $ = document.querySelectorAll.bind(document);
 Element.prototype.on = Element.prototype.addEventListener;
