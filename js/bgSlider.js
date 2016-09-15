366ed3ad7354cf3d37004e9e5b8db0437dab0e6f
bgthomasson <benthomonster@gmail.com>
2016-09-14 21:56:06 -0500

    bump

--- js/bgSlider.js
+++ js/bgSlider.js
@@ -1,48 +1,42 @@
+// riffing off this https://css-tricks.com/make-super-quick-little-slider-play-values/
+
 var range = document.querySelector("#range");
-var h1 = document.getElementsByTagName("body")[0];
-// just to mess around with selector options
-// yes I tested it on the h1 first, now testing other elements...
+var mutant = document.getElementsByTagName("body")[0];
 
 range.addEventListener("input", function() {
-  // here's the original example to make sure it works- it does for this: 
-  // h1.style["font-size"] = this.value + "em";
-  // OK cool now let's do something different and more complicated like changing colors...
-  // h1.style.background = "#123";
-  // Oh there is a very simple, fundamental thing to remember- input is TEXT by default,
-  // so all those numbers aren't INTEGERS to JS! So wrap in "parentheses".
 
   var toggle = this.value;
   
   switch (toggle) {
     case "1":
-      h1.style.background = "#fbfcf7"; // off white
+      mutant.style.background = "#fbfcf7"; // off white
       break;
     case "2":
-      h1.style.background = "#ffc0cb"; //pink
+      mutant.style.background = "#ffc0cb"; //pink
       break;
     case "3":
-      h1.style.background = "#ffc733"; //orange
+      mutant.style.background = "#ffc733"; //orange
       break;
     case "4":
-      h1.style.background = "#ffd700"; // gooooold
+      mutant.style.background = "#ffd700"; // gooooold
       break;
     case "5":
-      h1.style.background = "#98fb98"; // green
+      mutant.style.background = "#98fb98"; // green
       break;
     case "6":
-      h1.style.background = "#e0ffff"; // cyan 
+      mutant.style.background = "#e0ffff"; // cyan 
       break;
     case "7":
-      h1.style.background = "#b0e0e6"; // blue
+      mutant.style.background = "#b0e0e6"; // blue
       break;
     case "8":
-      h1.style.background = "#e6e6fa"; // purple
+      mutant.style.background = "#e6e6fa"; // purple
       break;
     case "9":
-      h1.style.background = "#dcdcdc"; //grey
+      mutant.style.background = "#dcdcdc"; //grey
       break;
     default:
-      h1.style.background = "#fbfcf7";
+      mutant.style.background = "#fbfcf7";
   }
   
 });
